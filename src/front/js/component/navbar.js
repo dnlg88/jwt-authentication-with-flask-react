3009c61fed89ee404178b608e885af884d41e7a2
Daniel Guzmán <dnlguzman@gmail.com>
2022-08-05 19:52:16 +0000

    initial commit

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,18 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <Link to="/">
+          <span className="navbar-brand mb-0 h1">Home</span>
+        </Link>
+        <div className="actions">
+          <Link to="/login">
+            <a>Login</a>
+          </Link>
+          <Link to="/signup">
+            <a>Signup</a>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,23 +4,55 @@ import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  return (
+    <section
+      className="pt-5 pb-5 mt-0 align-items-center d-flex bg-dark"
+      style={{
+        height: "100vh",
+        backgroundSize: "cover",
+        backgroundImage:
+          "url(https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-0.3.5&s=c3d0603820b595592d80f5a239938c67&auto=format&fit=crop&w=1500&q=80)",
+      }}
+    >
+      <div className="container-fluid">
+        <div className="row justify-content-center align-items-center d-flex text-center h-100">
+          <div className="col-12 col-md-8  h-50 ">
+            <h1 className="display-2  text-light mb-2 mt-5">
+              <strong>Authentication Demo</strong>
+            </h1>
+            <p className="lead  text-light mb-5">
+              Check out my login and sign-up pages
+            </p>
+            <p>
+              <a
+                href="#"
+                className="btn bg-success shadow-lg btn-round text-light btn-lg btn-rised"
+              >
+                Login
+              </a>
+            </p>
+            <p>
+              <a
+                href="#"
+                className="btn bg-primary shadow-lg btn-round text-light btn-lg btn-rised"
+              >
+                Signup
+              </a>
+            </p>
+
+            <div className="btn-container-wrapper p-relative d-block  zindex-1">
+              <a
+                className="btn btn-link btn-lg   mt-md-3 mb-4 scroll align-self-center text-light"
+                href="#"
+              >
+                <i className="fa fa-angle-down fa-4x text-light"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
